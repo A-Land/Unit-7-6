cc4ec51b8fa844ba10603e3d633b5cedbcd96396
Adam Landriault <>
2021-05-12 01:45:58 +0000

    fixed errors

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-var answer = 5 
-var guess = prompt ('what is your guess?')
-if (answer == guess) {
+const answer = 5
+const guess = prompt('what is your guess?')
+if (answer === guess) {
   console.log('you are correct')
-} else if (guess > answer) { 
-  console.log('your answer is too high')
-}
+} else if (guess > answer) {
+  console.log('your answer is too high')}
 else if (guess < answer) {
   console.log('your answer is too low')
 }
